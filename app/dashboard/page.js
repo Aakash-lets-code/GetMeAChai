64b616bfd3607c14d3bd9c0bad171e93929087d2
Aakash lets code <akashshrivastav071@gmail.com>
2025-01-20 10:39:38 +0530

    fix navigation in dashboard and decleration of hook

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,14 @@
 "use client"
 import React,{ useState } from 'react' 
 import { useSession, signIn, signOut } from "next-auth/react" 
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 
 const page = () => {
   const { data: session } = useSession()
+  const router = useRouter()
   
-      if (session) {
-           const router = useRouter()
+      if (!session) {
            router.push('/login')
       }
   
