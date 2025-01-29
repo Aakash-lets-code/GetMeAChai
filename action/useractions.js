b6ef7092b9b1ddd9683dfa1ddbdaa27af644161a
Aakash lets code <akashshrivastav071@gmail.com>
2025-01-29 17:40:09 +0530

    use razor pay integration

--- action/useractions.js
+++ action/useractions.js
@@ -4,3 +4,23 @@ import Razorpay from "razorpay"
 import Payment from "@/models/Payment"
 import connectDB from "@/db/connectDb"
 import User from "@/models/User"
+
+export const initiate = async (amount, to_username, paymentform) => {
+    await connectDB()
+    var instance = new Razorpay({ key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_SECRET' })
+    // var instance = new Razorpay({ key_id: process.env.KEY_ID, key_secret: process.env.KEY_SECERT }) // use this line when you have add the razorpay id and secert 
+
+    
+
+    let option = {
+        amount: Number.parseInt(amount),
+        currency: "INR",
+    }
+
+    let x = await instance.orders.create(options)
+
+    // create a payment object which shows a pending payment in the database
+    await Payment.create({oid: x.id, amount: amount, to_username: to_username, name: paymentform.name, message: paymentform.message  })
+    
+    return x;
+}
