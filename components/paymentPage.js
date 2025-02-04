ec2d5940f616364a823a6a32fae43bb6990c6ffd
Aakash lets code <akashshrivastav071@gmail.com>
2025-02-04 20:37:41 +0530

    convert paise into ruppees

--- components/paymentPage.js
+++ components/paymentPage.js
@@ -124,7 +124,7 @@ const paymentPage = ({ username }) => {
 
                         <input onChange={handleChange} value={paymentform.amount} name='amount' type="text" placeholder='Enter Amount' className='rounded-lg bg-slate-800 p-3 w-full' />
 
-                        <button onClick={() => { pay(paymentform.amount) }} type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay</button>
+                        <button onClick={() => { pay(paymentform.amount * 100) }} type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay</button>
                     </div>
 
                     {/* or choose form these amount */}
