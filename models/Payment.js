31657e6866d9929246ca4b85d50035fb1371a4d8
Aakash lets code <akashshrivastav071@gmail.com>
2025-02-06 19:51:05 +0530

    add razorpay id and secert

--- models/Payment.js
+++ models/Payment.js
@@ -9,6 +9,8 @@ const PaymentSchema = new Schema({
     amount: { type: Number, required: true },
     createdAt: { type: String, default: Date.now },
     updatedAt: { type: String, default: Date.now },
+    razorpayid: {type: string}, 
+    razorpaysecret: {type: string}, 
     done: { type: Boolean, default: false },
 });
  
