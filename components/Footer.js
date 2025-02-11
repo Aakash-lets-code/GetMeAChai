c0701f9380c25b295345cbd1844dd2f65a6017e5
Aakash lets code <akashshrivastav071@gmail.com>
2025-02-11 20:15:02 +0530

    add currentYear

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
+
+    const currentYear  = new Date().getFullYear();
+
     return (
         <footer className='bg-gray-900 text-white  '>
             <div className="  flex justify-center items-center py-4  ">
                 <p>
-                     Copyright © Get me A chai -All rights resereved
+                     Copyright © {currentYear} Get me A chai -All rights resereved
                 </p>
             </div>
         </footer>
