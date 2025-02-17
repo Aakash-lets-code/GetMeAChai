e413bd94b50146f03c503fbc622c2ddc882d4c8d
Aakash lets code <akashshrivastav071@gmail.com>
2025-02-17 19:33:08 +0530

    make minor responsiveness

--- components/Navbar.js
+++ components/Navbar.js
@@ -63,7 +63,7 @@ const Navbar = () => {
                 }
 
                 {!session && <Link href={"/login"}>
-                    <button className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ' >
+                    <button className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-1' >
                         Login
                     </button>
                 </Link>}
