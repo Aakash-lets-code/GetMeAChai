9189d6b668f55bfcf4ea316427c37cf216313730
Aakash lets code <akashshrivastav071@gmail.com>
2025-02-20 20:25:32 +0530

    work on responsiveness

--- app/page.js
+++ app/page.js
@@ -30,20 +30,20 @@ export default function Home() {
 
             <div className="bg-red-50 opacity-5 h-1"> </div>
 
-            <div className=" text-white container mx-auto py-5 px-10">
+            <div className=" text-white container mx-auto py-5 px-5 md:px-10">
                 <h2 className="text-center text-3xl font-bold  my-10" >Your fans can buy you a Chai</h2>
-                <div className="flex gap-5 justify-around">
-                    <div className="item flex items-center justify-center flex-col space-y-3">
+                <div className="flex flex-col md:flex-row gap-5 justify-around">
+                    <div className="item flex items-center justify-center flex-col space-y-1 md:space-y-3">
                         <img className=" bg-slate-600 rounded-full p-1 " src="/man.png" alt="man" width={80} />
                         <p className="font-bold mt-1 text-center ">Fund Yourself</p>
                         <p className="w-3/4 text-center">Your fans are avaiable to help you</p>
                     </div>
-                    <div className="item flex items-center justify-center flex-col space-y-3">
+                    <div className="item flex items-center justify-center flex-col space-y-1 md:space-y-3">
                         <img className=" bg-slate-600 rounded-full p-1 invert " src="/bitcoin.gif" alt="bitcoin" width={80} />
                         <p className="font-bold mt-1 text-center ">Fund Yourself</p>
                         <p className="w-3/4 text-center">Your fans are avaiable to help you</p>
                     </div>
-                    <div className="item flex items-center justify-center flex-col space-y-3">
+                    <div className="item flex items-center justify-center flex-col space-y-1 md:space-y-3">
                         <img className=" bg-slate-600 rounded-full p-1 " src="/group.png" alt="group" width={80} />
                         <p className="font-bold mt-1 text-center ">Your fans want to help</p>
                         <p className="w-3/4 text-center">Your fans are avaiable to help you</p>
