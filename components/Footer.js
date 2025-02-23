726ec2b03c3c041d647a22eb25789b2c673a9a9c
Aakash lets code <akashshrivastav071@gmail.com>
2025-02-23 18:37:05 +0530

    do minor changes

--- components/Footer.js
+++ components/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
     return (
         <footer className='bg-gray-900 text-white'>
             <div className="container mx-auto px-4 py-4 flex justify-center items-center">
-                <p className="text-center text-sm sm:text-base md:text-lg ">
-                    Copyright © {currentYear} Get me A chai - All rights reserved
+                <p className="text-center text-sm sm:text-base md:text-lg">
+                    &copy; {currentYear} Get me A chai - All rights reserved
                 </p>
             </div>
         </footer>
